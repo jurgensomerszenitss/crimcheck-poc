b20c00865d90dc2fca8f00134ff9aa8aac657dc2
Jurgen Somers <jurgen.somers@zenitss.eu>
2021-08-23 18:30:20 +0300

    lesson validation

---
+++ Grader.Api/Validations/LessonCreateCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Grader.Api.Business.Commands.LessonCreate;
+
+namespace Grader.Api.Validations
+{
+    public class LessonCreateCommandValidator : AbstractValidator<LessonCreateCommand>
+    {
+        public LessonCreateCommandValidator()
+        {
+            RuleFor(x => x.Topic).NotNull().NotEmpty().WithMessage("Topic is required");
+            RuleFor(x => x.Topic).MaximumLength(200).WithMessage("Topic cannot be longer than 200");
+            RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Description).MaximumLength(200).WithMessage("Description cannot be longer than 200");
+        }
+    }
+}
---
+++ Grader.Api/Validations/LessonUpdateCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Grader.Api.Business.Commands.LessonUpdate;
+
+namespace Grader.Api.Validations
+{
+    public class LessonUpdateCommandValidator : AbstractValidator<LessonUpdateCommand>
+    {
+        public LessonUpdateCommandValidator()
+        {
+            RuleFor(x => x.Topic).NotNull().NotEmpty().WithMessage("Topic is required");
+            RuleFor(x => x.Topic).MaximumLength(200).WithMessage("Topic cannot be longer than 200");
+            RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Description).MaximumLength(200).WithMessage("Description cannot be longer than 200");
+        }
+    }
+}
